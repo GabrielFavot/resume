cd09c91d2b75f993074aca42ae3c3c9b67668890
Gab <gabriel.favotwurtz@gmail.com>
2026-01-12 17:08:21 +0100

    refactor: streamline resume component structure by removing unnecessary ResumeSection wrappers

--- pages/index.vue
+++ pages/index.vue
@@ -10,22 +10,37 @@
           <ResumeHeader />
         </ResumeSection>
 
-        <ResumeWork />
-
-        <ResumeEducation />
-
-        <ResumeSkills />
-
-        <ResumeProjects />
-
-        <ResumeLanguages />
+        <ResumeSection v-if="hasWork">
+          <ResumeWork />
+        </ResumeSection>
+        <ResumeSection v-if="hasEducation">
+          <ResumeEducation />
+        </ResumeSection>
+        <ResumeSection v-if="hasSkills">
+          <ResumeSkills />
+        </ResumeSection>
+        <ResumeSection v-if="hasProjects">
+          <ResumeProjects />
+        </ResumeSection>
+        <ResumeSection v-if="hasLanguages">
+          <ResumeLanguages />
+        </ResumeSection>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+import { useResume } from '~/composables/useResume'
 
+const { work, education, skills, projects, languages } = await useResume()
+
+const hasWork = computed(() => work.value.length > 0)
+const hasEducation = computed(() => education.value.length > 0)
+const hasSkills = computed(() => skills.value.length > 0)
+const hasProjects = computed(() => projects.value.length > 0)
+const hasLanguages = computed(() => languages.value.length > 0)
 </script>
 
 <style></style>
